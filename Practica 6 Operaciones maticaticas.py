edeb04cb3e9bf357f4153c0261818a163f0a5056
Claudioafg <claudio.afg07>
2025-04-08 09:28:49 -0400

    update

---
+++ Practica 6 Operaciones maticaticas.py
@@ -0,0 +1,10 @@
+numero1=int (input("Introduce el primer número: "))
+numero2=int (input("Introduce el segundo número: "))  
+Suma=numero1+numero2
+Resta=numero1-numero2 
+Multiplicacion=numero1*numero2
+Division=numero1/numero2
+print("La suma es:", Suma)
+print("La resta es:", Resta)
+print("La multiplicación es:", Multiplicacion)
+print("La división es:", Division)
